567d7d85c00003b8216a9750951d4559eeff9ee8
FznFxp07 <fernando.goncalves.campos@escola.pr.gov.br>
2025-05-06 19:16:01 -0300

    salvar

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
     console.log('Isso ai! Você descobriu o número secreto (5)');
+} else {
+    alert('você errou :(')
 }
